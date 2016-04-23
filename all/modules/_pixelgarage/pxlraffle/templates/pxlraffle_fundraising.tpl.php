5b49720e2ccdca8cc68735e5a1002ccc1a32a795
Ralph Moser <ralph@ramosoft.ch>
2016-04-23 21:20:05 +0200

    - front side styling finished

--- all/modules/_pixelgarage/pxlraffle/templates/pxlraffle_fundraising.tpl.php
+++ all/modules/_pixelgarage/pxlraffle/templates/pxlraffle_fundraising.tpl.php
@@ -15,31 +15,33 @@
 <div id="<?php print $container_id; ?>">
   <!-- The current raffle section-->
   <div class="current-raffle">
-    <?php if (!empty($title)): ?>
-      <h2 class="block-header"><?php print $title; ?></h2>
-    <?php endif; ?>
-    <div class="odometer-container">
-      <?php if ($odometer_label_on_top): ?>
-        <div class="odometer-label"><?php print $odometer_label; ?></div>
-        <div class="odometer"><?php print $user_count; ?></div>
-      <?php else: ?>
-        <div class="odometer"><?php print $user_count; ?></div>
-        <div class="odometer-label"><?php print $odometer_label; ?></div>
+    <div class="inner-current-raffle">
+      <?php if (!empty($title)): ?>
+        <h2 class="block-header"><?php print $title; ?></h2>
       <?php endif; ?>
-    </div>
-    <div class="total-amount">CHF <?php print $total_amount; ?></div>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="<?php print $fill_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php print $fill_level; ?>%;">
-        <span class="sr-only"><?php print $fill_level; ?>% Complete</span>
+      <div class="odometer-container">
+        <?php if ($odometer_label_on_top): ?>
+          <div class="odometer-label"><?php print $odometer_label; ?></div>
+          <div class="odometer"><?php print $user_count; ?></div>
+        <?php else: ?>
+          <div class="odometer"><?php print $user_count; ?></div>
+          <div class="odometer-label"><?php print $odometer_label; ?></div>
+        <?php endif; ?>
       </div>
-    </div>
-    <div class="current-amount-container">
-      <span class="fa fa-caret-up" style="position: relative; left: <?php print $fill_level; ?>%;"></span>
-      <div class="current-amount"  style="position: relative; left: <?php print $fill_level; ?>%;">CHF <?php print $current_amount; ?></div>
-    </div>
-    <div class="explanation"><?php print $explanation; ?></div>
-    <div class="actions">
-      <a class="btn btn-default link-participate" href="<?php print $url_participate; ?>"><?php print $label_participate; ?></a>
+      <div class="total-amount">CHF <?php print $total_amount; ?></div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="<?php print $fill_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php print $fill_level; ?>%;">
+          <span class="sr-only"><?php print $fill_level; ?>% Complete</span>
+        </div>
+      </div>
+      <div class="current-amount-container">
+        <span class="fa fa-caret-up" style="position: relative; left: <?php print $fill_level; ?>%;"></span>
+        <div class="current-amount"  style="position: relative; left: <?php print $fill_level; ?>%;">CHF <?php print $current_amount; ?></div>
+      </div>
+      <div class="explanation"><?php print $explanation; ?></div>
+      <div class="actions">
+        <a class="btn btn-default link-participate" href="<?php print $url_participate; ?>"><?php print $label_participate; ?></a>
+      </div>
     </div>
   </div>
 </div>
