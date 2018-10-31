5123930671e5e54c84fa6cd2a6270fd66c7e4cfb
Ralph Moser <ralph@ramosoft.ch>
2018-10-31 23:01:52 +0100

    - new styling for survey pages - general improvements of styling

---
+++ all/themes/pixelgarage/templates/bootstrap-3-6-3--paragraphs-item-image-text-image.tpl.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * Bootstrap 3-6-3 template for Display Suite.
+ */
+$url1 = $field_link_1[0]['url'];
+$url2 = $field_link_2[0]['url'];
+?>
+
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <div class="row">
+    <<?php print $central_wrapper; ?> class="col-sm-6 col-sm-push-3 <?php print $central_classes; ?>">
+      <?php print $central; ?>
+    </<?php print $central_wrapper; ?>>
+    <<?php print $left_wrapper; ?> class="col-xxs-6-sm-3 col-sm-pull-6 <?php print $left_classes; ?>">
+      <?php if (!empty($url1)): ?>
+        <a href="<?php print $url1; ?>"><?php print $left; ?></a>
+      <?php else: ?>
+        <?php print $left; ?>
+      <?php endif; ?>
+    </<?php print $left_wrapper; ?>>
+    <<?php print $right_wrapper; ?> class="col-xxs-6-sm-3 <?php print $right_classes; ?>">
+      <?php if (!empty($url2)): ?>
+        <a href="<?php print $url2; ?>"><?php print $right; ?></a>
+      <?php else: ?>
+        <?php print $right; ?>
+      <?php endif; ?>
+    </<?php print $right_wrapper; ?>>
+  </div>
+</<?php print $layout_wrapper ?>>
+
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
