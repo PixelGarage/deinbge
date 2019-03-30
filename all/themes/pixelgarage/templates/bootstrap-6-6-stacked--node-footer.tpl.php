1bb80c3f7e8f041ac825882e110a9d220b6792c6
ralphmoser <ralph@ramosoft.ch>
2019-03-30 01:11:03 +0100

    [Refactor] footer styled

---
+++ all/themes/pixelgarage/templates/bootstrap-6-6-stacked--node-footer.tpl.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ * Bootstrap 6-6 stacked template for Display Suite.
+ */
+?>
+
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
+<?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+<?php endif; ?>
+<?php if ($top): ?>
+  <div class="row">
+    <<?php print $top_wrapper; ?> class="col-sm-12 <?php print $top_classes; ?>">
+      <?php print $top; ?>
+    </<?php print $top_wrapper; ?>>
+  </div>
+<?php endif; ?>
+<?php if ($left || $right): ?>
+  <div class="row">
+    <<?php print $left_wrapper; ?> class="column col-xs-12 col-md-6 left <?php print $left_classes; ?>">
+      <div class="inner-column col-xs-12 col-sm-6">
+        <?php print render($content['field_medienkontakt']); ?>
+      </div>
+      <div class="inner-column col-xs-12 col-sm-6">
+        <?php print render($content['field_text']); ?>
+      </div>
+    </<?php print $left_wrapper; ?>>
+
+    <<?php print $right_wrapper; ?> class="column col-xs-12 col-md-6 right <?php print $right_classes; ?>">
+      <div class="inner-column col-xs-12 col-sm-6">
+        <?php print render($content['body']); ?>
+      </div>
+      <div class="inner-column col-xs-12 col-sm-6">
+        <?php print render($content['follow_site']); ?>
+      </div>
+    </<?php print $right_wrapper; ?>>
+
+  </div>
+<?php endif; ?>
+<?php if ($bottom): ?>
+  <div class="row">
+    <<?php print $bottom_wrapper; ?> class="col-sm-12 <?php print $bottom_classes; ?>">
+      <?php print $bottom; ?>
+    </<?php print $bottom_wrapper; ?>>
+  </div>
+<?php endif; ?>
+</<?php print $layout_wrapper ?>>
+
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
